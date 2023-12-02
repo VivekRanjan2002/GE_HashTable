895b06f8327eb2c320a46fca83faf160429f7758
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-03 03:26:34 +0530

    Added:MyMapNode class

---
+++ MyMapNode.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class MyMapNode<K,V> {
+public K key=null;
+public V value=null;
+
+
+public MyMapNode(K key,V value){
+    this.key=key;
+    this.value=value;
+}
+
+}
